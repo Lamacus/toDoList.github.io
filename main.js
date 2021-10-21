7ee678bb3e3e199b139c9f02ec261a5f74c12519
Lamacus <hoanglocdalat@gmail.com>
2021-10-21 16:25:12 +0700

    todolist

---
+++ main.js
@@ -0,0 +1,25 @@
+let listTodo = [
+    {
+        work: 'Hoc lap trinh',
+        date: 'abc',
+    },
+    {
+        work: 'Hoc JS',
+        date: 'abc',
+    },
+    {
+        work: 'Hoc Ruby',
+        date: 'asgasg',
+    }
+]
+
+for(let i = 0; i < listTodo.length; i++) {
+    let divTable = `
+    <tr>
+        <td><input type="checkbox"></td>
+        <td>${listTodo[i].work}</td>
+        <td>${listTodo[i].date}</td>
+    </tr>
+    `
+    $('.content-table').append(divTable)
+}
