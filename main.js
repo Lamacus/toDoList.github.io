c0d38140bf98c7aa5172b0d2a92bec680c2b5e0e
Lamacus <hoanglocdalat@gmail.com>
2021-10-22 18:04:08 +0700

    todolist

--- main.js
+++ main.js
@@ -1,31 +1,105 @@
+var msg  = document.title;
+var speed = 500;
+var endChar = " ";
+var pos = 0;
+
+function moveTitle() {
+     var ml = msg.length;
+    title = msg.substr(pos,ml) + endChar + msg.substr(0,pos);
+    document.title = title;
+    pos++;
+    if (pos > ml) pos=0;
+    window.setTimeout("moveTitle()",speed);
+}
+
+moveTitle();
+
 let listTodo = [
     {
-        work: 'Hoc lap trinh',
-        date: 'abc',
+        work: 'Học online',
     },
     {
-        work: 'Hoc JS',
-        date: 'abc',
+        work: 'Nấu cơm'
     },
     {
-        work: 'Hoc Ruby',
-        date: 'asgasg',
+        work: 'Làm bài tập',
     }
 ]
 
-for(let i = 0; i < listTodo.length; i++) {
-    let divTable = `
-    <tr>
-        <td><input type="checkbox"></td>
-        <td>${listTodo[i].work}</td>
-        <td>${listTodo[i].date}</td>
-    </tr>
-    `
-    $('.content-table').append(divTable)
+function render() {
+    $('.content-table').html('')
+    for(let i = 0; i < listTodo.length; i++) {
+        let divTable = `
+        <tr>
+            <td>${listTodo[i].work}</td>
+            <td><button index="${i}" type="button" class="modify btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Modify</button>
+            <td><button index="${i}" class="delete">Delete</button>
+        </tr>
+        `
+        $('.content-table').append(divTable)
+    }    
+    $('input[type="checkbox"]').on('change', function(){
+        if (this.checked) {
+            $('td').css({'text-decoration': 'line-through'})
+            $('.modify').css({"background": 'grey'})
+            $('.delete').css({"background": 'grey'})
+        }
+        else {
+            $('td').css({"text-decoration": 'none'})
+            $('.modify').css({"background": '#1471CD'})
+            $('.delete').css({"background": '#ff5722'})
+        }
+    })
+
+    // $(document).ready(function(){
+    //     $('input[type="checkbox"]').on('click', function() {
+    //         var index = $('input[type="checkbox"]:checked')
+    //         if(index.length > 0) {
+    //                 $('td').css({'text-decoration': 'line-through'})
+    //                 $('.modify').css({"background": 'grey'})
+    //                 $('.delete').css({"background": 'grey'})
+    //         } else {
+    //             $('td').css({"text-decoration": 'none'})
+    //             $('.modify').css({"background": '#1471CD'})
+    //             $('.delete').css({"background": '#ff5722'})
+    //         }
+    //     })
+    // })
+
+    $('#deleteAll').on('click', function() {
+        let index = $(this).attr('index')
+        listTodo.splice(index, listTodo.length)
+        render()
+    })
+    $('.delete').on('click', function() {
+        let index = $(this).attr('index')
+        listTodo.splice(index, 1)
+        render()
+    })
+    $('.modify').on('click', function() {
+        let index = $(this).attr('index')
+        $('button[confirm="confirm"]').attr('index', index)
+    })
 }
 
 $('#button').on('click', function(){
-    let button = $('#typeTask').val()
-    console.log(button)
+    let work = $('#typeTask').val()
+    if (work === '') {
+        $('#button').off('click', function() {
+            
+        })
+    } else listTodo.unshift({work})
+    render()
 })
+
+$('button[confirm="confirm"]').on('click', function() {
+    let newTypeTask = $('#newTypeTask').val()
+    let index = $(this).attr('index')
+    listTodo[index].work = newTypeTask
+    render()
+})
+
+render()
+
+
 
